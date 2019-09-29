4b612ebe562bbebac876bde762bb69b4b3ece197
Munmun Shrestha <munmun.shrestha@outlook.com>
2019-09-29 01:01:55 -0500

    working feedback form

--- feedbackForm.php
+++ feedbackForm.php
@@ -12,24 +12,46 @@ new_header("FeedbackForm");
 $mysqli = Database::dbConnect();
 $mysqli->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
 if (($output = message()) !== null) {
     echo $output;
 }
 
+?>
+<script type="text/javascript">
+    function checkCaptcha() {
+        // var ch = document.getElementsByName("feedback[]");
+      
+            //checking if recaptcha is checked
+            var response = grecaptcha.getResponse();
+            //response.length=0 if not checked
+            if (response.length == 0) {
+                alert("The form is not completely filled.");
+                return false;
+            } else {
+                return true;
+                //when it return true- your form will  submit and will  redirect
+            }
+
+        // }
+    }
+</script>
+<?php
 if (isset($_POST["submit"])) {
 
-    if ($_POST["g-recaptcha-response"]) {
-        $response = $reCaptcha->verifyResponse(
-            $_SERVER["REMOTE_ADDR"],
-            $_POST["g-recaptcha-response"]
-        );
-    }
+    // if ($_POST["g-recaptcha-response"]) {
+    //     $response = $reCaptcha->verifyResponse(
+    //         $_SERVER["REMOTE_ADDR"],
+    //         $_POST["g-recaptcha-response"]
+    //     );
+    // }
 
-    if ((isset($_POST["feedback"]) && count($_POST["feedback"]) != 0) && ($response != null && $response->success)) {
-        $feedback_array= $_POST['feedback'];
-         echo count($_POST["feedback"]);      
-        foreach($feedback_array as $feedback_text){
-            if ($feedback_text!=""){
+    // if ((isset($_POST["feedback"]) && count($_POST["feedback"]) != 0) && ($response != null && $response->success)) {
+    if (isset($_POST["feedback"]) ) {
+        $feedback_array = $_POST['feedback'];
+        $count=0;     
+        foreach ($feedback_array as $feedback_text) {
+            if ($feedback_text != "") {
                 //Create and prepare query to insert information that has been posted
                 $query = "INSERT INTO HP_FEEDBACK (FEEDBACK_TEXT) VALUES (?)";
                 // Execute query
@@ -41,40 +63,77 @@ if (isset($_POST["submit"])) {
                     $_SESSION["message"] = "ERROR! Could not submit feedback";
                 }
             }
+            else{
+                $count++;
+            }
         }
-    } else {
-        $_SESSION["message"] = "The form is not completely filled";
-    }
+        if ($count==count($feedback_array)){
+            $_SESSION["message"] = "You must enter a feedback to submit";
+        }
+    } 
+    
 
 
-    // redirect("feedbackForm.php");
+    redirect("feedbackForm.php");
 } else {
     ?>
     <div class="container">
         <h2> Feedback Form</h2>
 
         <h5> Your feedback is anonymous so feel free to write about anything related to our Computer Science Department. </h5>
-        <h5> You can provide both positive feedback and constructive criticism to the department. <b> Please be professional.</b></h5>
-        <form method="POST" action="feedbackForm.php" id="my_captcha_form">
+        <h5> You can provide both positive feedback and constructive criticism to the department. Please keep each feedback specific to one topic and <b> please be professional.</b></h5>
+        <iframe name="votar" style="display:none;"></iframe>
+        <form method="POST" name="form" action="feedbackForm.php" id="my_captcha_form" onsubmit="return checkCaptcha()">
             <div class="field_wrapper">
-                <div><label for="comment">Enter your feedback below:</label></div>
-                    
-                <textarea class="form-control" rows="5" id="comment" name="feedback[]"></textarea>
+                <div class="col-md-4"><label for="comment">Enter your feedback below:</label></div>
+                <div class="col-md-4 col-md-offset-4"><a href="javascript:void(0);" class="add_button pull-right" title="Add field"><img src="add.ico" width="18" height="18">
+Add Another Feedback</a></div>
+                <textarea class="form-control" rows="5" id="feedbackid" name="feedback[]"></textarea>
+                <p></p>
                 <?php add_textField(); ?>
-                <a href="javascript:void(0);" class="add_button pull-right" title="Add field">Add Another Feedback</a>
             </div>
-   
+
             <p>
                 <div class="g-recaptcha" data-sitekey="6Le2L7gUAAAAAGP7U9D40nU-kN0xbQRdUK9aWdOk"></div>
             </p>
-            <p></p>
+            <p><button type="submit" class="btn btn-default" name="submit">Submit</button></p>
 
-            <button type="submit" class="btn btn-default" name="submit">Submit</button>
-            
         </form>
     </div>
+
 <?php
 }
+
+function add_textField()
+{
+    ?>
+    <script>
+        $(document).ready(function() {
+            var maxField = 10; //Input fields increment limitation
+            var addButton = $('.add_button'); //Add button selector
+            var wrapper = $('.field_wrapper'); //Input field wrapper
+            var fieldHTML = "<div><textarea class='form-control' rows='5' id='feedbackid' name='feedback[]'></textarea><a href='javascript:void(0);' class='remove_button pull-right'><img src='delete.jpeg' width='18' height='18'>Delete</a></div>"; //New input field html 
+            var x = 1; //Initial field counter is 1
+
+            //Once add button is clicked
+            $(addButton).click(function() {
+                //Check maximum number of input fields
+                if (x < maxField) {
+                    x++; //Increment field counter
+                    $(wrapper).append(fieldHTML); //Add field html
+                }
+            });
+
+            //Once remove button is clicked
+            $(wrapper).on('click', '.remove_button', function(e) {
+                e.preventDefault();
+                $(this).parent('div').remove(); //Remove field html
+                x--; //Decrement field counter
+            });
+        });
+    </script>
+<?php  }
+
 new_footer();
 $stmt = NULL;
 Database::dbDisconnect();
