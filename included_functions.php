4088aea8b19f9b7e06692f8f110b7c3ed4acd9be
Munmun Shrestha <munmun.shrestha@outlook.com>
2019-09-29 01:12:23 -0500

    working feedback form

--- included_functions.php
+++ included_functions.php
@@ -22,37 +22,7 @@ function new_header($title)
         </head>
         <body>";
 }
-
-function add_textField(){
-	?>
-	<script type='text/javascript'>
-			  $(document).ready(function(){
-				  var maxField = 10; //Input fields increment limitation
-				  var addButton = $('.add_button'); //Add button selector
-				  var wrapper = $('.field_wrapper'); //Input field wrapper
-				  var fieldHTML = "<div><textarea class='form-control' rows='5' id='comment' name='feedback[]'></textarea><a href='javascript:void(0);' class='remove_button pull-right'>Delete</a></div>"; //New input field html 
-				  var x = 1; //Initial field counter is 1
-				  
-				  //Once add button is clicked
-				  $(addButton).click(function(){
-					  //Check maximum number of input fields
-					  if(x < maxField){ 
-						  x++; //Increment field counter
-						  $(wrapper).append(fieldHTML); //Add field html
-					  }
-				  });
-				  
-				  //Once remove button is clicked
-				  $(wrapper).on('click', '.remove_button', function(e){
-					  e.preventDefault();
-					  $(this).parent('div').remove(); //Remove field html
-					  x--; //Decrement field counter
-				  });
-			  });
-			  </script>
-			  <?php
-			}
-		
+	
 function new_footer()
 {
 	echo "</body>";
